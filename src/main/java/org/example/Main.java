71bc152b8f82e144355b1e4bb8bf0dd444b97182
wulandharmaa <145654637+wulandharmaa@users.noreply.github.com>
2024-10-23 00:05:18 +0700

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+
+        for(int i = 0; i < 2; ++i) {
+            System.out.println("" + i + ". Hai");
+        }
+
+    }
+}
